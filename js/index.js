8d4503209d1c61afba2fe845c5c909292dfc5f9b
AuguSarachaga <cascasarach@gmail.com>
2023-06-27 17:31:21 -0300

    JS a login

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+let wrapper = document.querySelector('.wrapper');
+let loginLink = document.querySelector('.login-link');
+let registerLink = document.querySelector('.register-link');
+let btnPopup = document.querySelector('.btnLogin-popup');
+let iconClose = document.querySelector('.icon-close');
 
 
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -14,9 +17,30 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+let emailLogin = document.getElementById('emailLogin');
+let passLogin = document.getElementById('passLogin');
+let loginForm = document.getElementById('loginForm');
+
+const userLogin = (event) => {
+  event.preventDefault();
+
+  let email = emailLogin.value;
+  let password = passLogin.value;
+
+
+
+ 
+  if (email === 'usuario@example.com' && password === 'contrasena') {
+    console.log('Inicio de sesión exitoso');
+
+  } else {
+    console.log('Inicio de sesión fallido');
+
+  }
+};
+
+loginForm.addEventListener('submit', userLogin);
